0526b0cff76b51f5df53a02a0dcffa22aa4a176a
Nicholas Walton <nicholas.walton@gmail.com>
2024-08-05 15:54:01 -0700

    Test that Fibonacci sequence is non-decreasing

---
+++ test_fib.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestFibonacciSequence(unittest.TestCase):
+    def setUp(self):
+        with open("fib.txt") as f:
+            self.values = [int(line) for line in f.readlines()]
+
+    def test_increasing(self):
+        pairs = list(zip(self.values[:-1], self.values[1:]))
+        self.assertEqual(len(pairs), len(self.values) - 1)
+        for smaller, larger in pairs:
+            self.assertLessEqual(smaller, larger)
+
+
+if __name__ == "__main__":
+    unittest.main()
